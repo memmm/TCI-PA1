99899973e5078b0e60ac685089307cc0926e2ba5
memmm <emesenge@gmail.com>
2019-11-28 23:33:46 +0100

    Test for constructor and addDoubleToData

--- src/test/java/BasicStatisticTest.java
+++ src/test/java/BasicStatisticTest.java
@@ -1,8 +1,25 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BasicStatisticTest {
 
-
+    public void nrOfItemsWhenNoneArePresentIsZero() {
+        //arrange
+        BasicStatisticInterface bs = new BasicStatistic();
+        //act
+        int nrOfItems = bs.numberOfDataItems();
+        //assert
+        Assert.assertSame(0, nrOfItems);
+    }
+    public void nrOfItemsAfterAddingIsNotZero() {
+        //arrange
+        BasicStatisticInterface bs = new BasicStatistic();
+        //act
+        bs.addDoubleToData(2.42);
+        int nrOfItems = bs.numberOfDataItems();
+        //assert
+        Assert.assertNotSame(0, nrOfItems);
+    }
 }
