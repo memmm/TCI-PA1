cac721cdea6a826e6604520ba583ae77618e6d3d
memmm <emesenge@gmail.com>
2019-11-28 23:08:25 +0100

    Changed MyFirstClass to BasicStatistic

---
+++ src/main/java/BasicStatistic.java
@@ -0,0 +1,28 @@
+/**
+ * very simple implementation of the BasicStatisticInterface
+ */
+public class BasicStatistic implements BasicStatisticInterface {
+
+    @Override
+    public void addDoubleToData(Double valueToAdd){
+
+    }
+
+    @Override
+    public void clearData(){
+
+    }
+
+    @Override
+    public int numberOfDataItems(){
+        int rv = 0;
+        return rv;
+    }
+
+    @Override
+    public Double sum(){
+        double rv = 0.0;
+        return rv;
+    }
+
+}
